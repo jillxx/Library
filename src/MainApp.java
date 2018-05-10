aadb83757d2ca0b3d6e2d65528173a5c74a12289
brentcop <coppinger.brent@student.oaklandcc.edu>
2018-05-10 11:54:01 -0400

    Brent's commit

--- src/MainApp.java
+++ src/MainApp.java
@@ -3,7 +3,7 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!\nWorld: Goodbye");
-
+		System.out.println("this is me shuvo");
 		System.out.println("Hello my name is Brent!");
 	}
 

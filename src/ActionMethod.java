ae707c3ba6290c67be45a24bd80f82667af2fd61
jillxx <jiaoxu84@gmail.com>
2018-05-14 15:31:14 -0400

    function complete

--- src/ActionMethod.java
+++ src/ActionMethod.java
@@ -16,7 +16,8 @@ public class ActionMethod {
 	static Scanner scan = new Scanner(System.in);
 	static ArrayList<String> rentList = new ArrayList<String>();
 	static ArrayList<String> wholeBook = new ArrayList<String>();
-	
+	static ArrayList<String> search = new ArrayList<String>();
+
 	public static void wholeArrayAdd() {
 		Path readFile = Paths.get("library/book.txt");
 		File file = readFile.toFile();// convert to a file object.
@@ -27,9 +28,8 @@ public class ActionMethod {
 			BufferedReader reader = new BufferedReader(fr);// read blocks of info
 
 			String line = reader.readLine();
-			int counter = 1;
+
 			while (line != null) {
-				String[] temp = line.split(",");
 				wholeBook.add(line);
 				line = reader.readLine();
 			}
@@ -40,8 +40,6 @@ public class ActionMethod {
 			System.out.println("The system is crashed, please try again");
 		}
 	}
-		
-	
 
 	public static void readFromBookList() {
 		Path readFile = Paths.get("library/book.txt");
@@ -53,15 +51,17 @@ public class ActionMethod {
 			BufferedReader reader = new BufferedReader(fr);// read blocks of info
 
 			String line = reader.readLine();
-			System.out.printf("     %1$-50s %2$-20s %3$-10s %4$-15s %5$-15s", "BookTitle", "Author", "BookID", "Status","Due Date");
+			System.out.printf("     %1$-50s %2$-20s %3$-10s %4$-15s %5$-15s", "BookTitle", "Author", "BookID", "Status",
+					"Due Date");
 			System.out.println();
 			int counter = 1;
 			while (line != null) {
 				String[] temp = line.split(",");
-				//wholeBook.add(line); this is adding the list to an array which we can modify the txt file
+				// wholeBook.add(line); this is adding the list to an array which we can modify
+				// the txt file
 				Book b = new Book(temp[1], temp[2], temp[3], counter, temp[0]);
 				System.out.print(b);// this is printing the whole list
-				if (temp.length>4) {
+				if (temp.length > 4) {
 					System.out.print(temp[5]);
 				}
 				System.out.println();
@@ -95,15 +95,18 @@ public class ActionMethod {
 				if (bookID == borrowInput) {
 					if (!temp1[3].equals(Status.CheckedOut.toString())) {
 						temp1[3] = Status.CheckedOut.toString();
-						rentList.add(temp1[0] + "," + temp1[1] + "," + temp1[2] + "," + temp1[3] + "," + user + ","
-								+ LocalDate.now().plusWeeks(2));
+						// rentList.add(temp1[0] + "," + temp1[1] + "," + temp1[2] + "," + temp1[3] +
+						// "," + user + ","
+						// + LocalDate.now().plusWeeks(2));
 						wholeBook.set(i, temp1[0] + "," + temp1[1] + "," + temp1[2] + "," + temp1[3] + "," + user + ","
-								+ LocalDate.now().plusWeeks(2));
+								+ LocalDate.now().plusWeeks(2));        
+
 
 					} else {
 						System.out.println(temp1[1] + " has been checked out by another person.");
 					}
 				}
+				
 				outW.println(wholeBook.get(i));
 			}
 
@@ -119,7 +122,7 @@ public class ActionMethod {
 			if (!file1.renameTo(file)) {
 				System.out.println("Could not rename file");
 			}
-		//	System.out.println(rentList.size() + " items has been checked out.");
+			// System.out.println(rentList.size() + " items has been checked out.");
 			// System.out.println(rentList.get(1));
 
 		} catch (IOException e) {
@@ -141,40 +144,32 @@ public class ActionMethod {
 			BufferedReader reader = new BufferedReader(fr);// read blocks of info
 
 			String line = reader.readLine();
-			System.out.printf("     %1$-50s %2$-20s %3$-10s %4$-10s", "BookTitle", "Author", "BookId","Status");
+			System.out.printf("     %1$-50s %2$-20s %3$-10s %4$-15s %5$-15s", "BookTitle", "Author", "BookID", "Status",
+					"Due Date");
 			System.out.println();
 			int counter = 1;
-			ArrayList<String> searchAuthor = new ArrayList<String>();
+
 			while (line != null) {
-				//wholeBook.add(line);
+				// wholeBook.add(line);
 				String[] tempA = line.split(",");
 				Book ba = new Book(tempA[1], tempA[2], tempA[3], counter, tempA[0]);
 				String[] tempAuthor = tempA[2].toLowerCase().split("[ :.,?!]+");
 
 				if (Arrays.asList(tempAuthor).contains(author.trim().toLowerCase())) {
-					System.out.println(ba);
+					System.out.print(ba);
+					if (tempA.length > 4) {
+						System.out.print(tempA[5]);
+					}
+					System.out.println();
 
 					String b1 = tempA[1] + "," + tempA[2] + "," + tempA[3] + "," + tempA[0];
 					counter++;
-					searchAuthor.add(b1);
+					search.add(b1);
 				}
 
 				line = reader.readLine();
 			}
 			reader.close();// close the file reader too
-
-			System.out.println("\nPlease enter the bookID number you want to borrow");
-			String bookANum = scan.nextLine();
-			scan.close();
-			for (int i = 0; i < searchAuthor.size(); i++) {
-				String[] tempRent = searchAuthor.get(i).split(",");
-
-				if (bookANum.equalsIgnoreCase(tempRent[3])) {
-					int bc = Integer.parseInt(bookANum);
-					checkOut(userID, bc);
-					rentList.add(searchAuthor.get(i));
-				}
-			}
 
 		} catch (
 
@@ -196,80 +191,73 @@ public class ActionMethod {
 			BufferedReader reader = new BufferedReader(fr);// read blocks of info
 
 			String line = reader.readLine();
-			System.out.printf("     %1$-50s %2$-20s %3$-10s %4$-10s", "BookTitle", "Author", "BookId", "Status");
+			System.out.printf("     %1$-50s %2$-20s %3$-10s %4$-15s %5$-15s", "BookTitle", "Author", "BookID", "Status",
+					"Due Date");
 			System.out.println();
 			int counter = 1;
-			ArrayList<String> searchKeyword = new ArrayList<String>();
+
 			while (line != null) {
-				//wholeBook.add(line);// adding whole book list to the arraylist
 				String[] temp = line.split(",");
 
 				Book b = new Book(temp[1], temp[2], temp[3], counter, temp[0]);
 				String[] tempTitle = temp[1].toLowerCase().split("[ :.,?!]+");
 
 				if (Arrays.asList(tempTitle).contains(keyword.trim().toLowerCase())) {
-					System.out.println(b);
+					System.out.print(b);
+					if (temp.length > 4) {
+						System.out.print(temp[5]);
+					}
+					System.out.println();
 
 					String b1 = temp[1] + "," + temp[2] + "," + temp[3] + "," + temp[0];
 					counter++;
-					searchKeyword.add(b1);
+					search.add(b1);
 				}
 
 				line = reader.readLine();
 			}
 			reader.close();// close the file reader too
 
-			System.out.println("\nPlease enter the bookID number you want to borrow");
-			String bookChoice = scan.nextLine();
-
-			for (int i = 0; i < searchKeyword.size(); i++) {
-
-				String[] tempRent = searchKeyword.get(i).split(",");
-
-				if (bookChoice.equalsIgnoreCase(tempRent[3])) {
-					int bc = Integer.parseInt(bookChoice);
-					checkOut(userID, bc);
-					rentList.add(searchKeyword.get(i));
-				}
-
-			}
-
-			// System.out.println(rentList);
 		} catch (IOException e) {
 			System.out.println("The system is crashed, please try again");
 		}
 
 	}
 
+	// =============================================================================================
+	public static void searchCheckout(int bookIDNum, String userID) {
+		for (int i = 0; i < search.size(); i++) {
+			String[] tempRent = search.get(i).split(",");
+
+			int bookID = Integer.parseInt(tempRent[3]);
+			if (bookIDNum == bookID) {
+				checkOut(userID, bookIDNum);
+			}
+
+		}
+	}
+
 	// ========================================================================================
 	public static void pullRentList(String userID) {
-		Path readFile = Paths.get("library/book.txt");// get the path of the file
-		File file = readFile.toFile();// convert to a file object.
+		System.out.println("You have the following book.");
+		System.out.printf("     %1$-50s %2$-20s %3$-10s %4$-15s %5$-15s", "BookTitle", "Author", "BookID", "Status",
+				"Due Date");
+		System.out.println();
+		int counterR = 1;
+		for (int i = 0; i < wholeBook.size(); i++) {
+			String[] tempR = wholeBook.get(i).split(",");
 
-		
-			//FileReader fr = new FileReader(file);// read char to char
-			//BufferedReader reader = new BufferedReader(fr);// read blocks of info
-
-			
-			/*while (line != null) {
-				wholeBook.add(line);
-				line = reader.readLine();
-			}*/
-			System.out.printf("     %1$-50s %2$-20s %3$-10s %4$-10s", "BookTitle", "Author", "Status", "BookId");
-			System.out.println();
-			int counterR = 1;
-			for (int i = 0; i < wholeBook.size(); i++) {
-				String[] tempR = wholeBook.get(i).split(",");
-
-				if (tempR[3].equals(Status.CheckedOut.toString()) && (userID.equalsIgnoreCase(tempR[4].toString()))) {// FIXME
-																														// exception
-																														// throwed
-					Book br = new Book(tempR[1], tempR[2], tempR[3], counterR, tempR[0]);
-					System.out.println(br);
-					counterR++;
+			if (tempR[3].equals(Status.CheckedOut.toString()) && (userID.equalsIgnoreCase(tempR[4].toString()))) {
+				Book br = new Book(tempR[1], tempR[2], tempR[3], counterR, tempR[0]);
+				System.out.print(br);
+				if (tempR.length > 4) {
+					System.out.print(tempR[5]);
 				}
-			}}
-		
+				System.out.println();
+				counterR++;
+			}
+		}
+	}
 
 	// =========================================================
 	public static void returnMethod(String userID, int bookID) {
@@ -312,7 +300,7 @@ public class ActionMethod {
 			if (!file1.renameTo(file)) {
 				System.out.println("Could not rename file");
 			}
-			System.out.println(rentList.size() + " item(s) have been returned out.");
+			// System.out.println(rentList.size() + " item(s) have been returned out.");
 			// System.out.println(rentList.get(1));
 
 		} catch (IOException e) {
@@ -320,8 +308,6 @@ public class ActionMethod {
 		}
 
 	}
-
-
 
 	public static void addBook(String userString) {
 		Path readFile = Paths.get("library/book.txt");// get the path of the file
@@ -333,33 +319,33 @@ public class ActionMethod {
 			FileReader fr = new FileReader(file);// read char to char
 			BufferedReader reader = new BufferedReader(fr);// read blocks of info
 			PrintWriter outW = new PrintWriter(new FileOutputStream(file1));
-			
-		String[] temp1 = wholeBook.get(wholeBook.size()-1).split(",");
-		int newBookID = Integer.parseInt(temp1[0]);
-		newBookID++;
-		wholeBook.add(newBookID +"," + userString + "," + Status.OnShelf);
-		System.out.println("You added :" + userString);
-		for (int i = 0; i < wholeBook.size(); i++) {
-			
-			outW.println(wholeBook.get(i));
+
+			String[] temp1 = wholeBook.get(wholeBook.size() - 1).split(",");
+			int newBookID = Integer.parseInt(temp1[0]);
+			newBookID++;
+			wholeBook.add(newBookID + "," + userString + "," + Status.OnShelf);
+			System.out.println("You added :" + userString);
+			for (int i = 0; i < wholeBook.size(); i++) {
+
+				outW.println(wholeBook.get(i));
+			}
+
+			outW.close();
+			reader.close();
+
+			if (!file.delete()) {// delete the old booklist file
+				System.out.println("Could not delete file");
+				return;
+			}
+
+			// Rename the temp file book.txt
+			if (!file1.renameTo(file)) {
+				System.out.println("Could not rename file");
+			}
+
+		} catch (IOException e) {
+			System.out.println("Something went wrong");
 		}
-
-		outW.close();
-		reader.close();
-
-		if (!file.delete()) {// delete the old booklist file
-			System.out.println("Could not delete file");
-			return;
-		}
-
-		// Rename the temp file book.txt
-		if (!file1.renameTo(file)) {
-			System.out.println("Could not rename file");
-		}
-
-	} catch (IOException e) {
-		System.out.println("Something went wrong");
-	}
 	}
 
 }
--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -11,16 +11,17 @@ public class LibraryApp {
 		String userID = Validator.getString(scan, "Please enter your name: ");
 		int userChoice = 0;
 		ActionMethod.wholeArrayAdd();
+		
 		while (userChoice != 5) {
-		System.out.print(userID + ", ");
-		System.out.println("Please choose from the options below:");
-		System.out.println("1. Search by category");
-		System.out.println("2. Display our inventory");
-		System.out.println("3. Return");
-		System.out.println("4. Donate book");
-		System.out.println("5. Quit");
+			System.out.print(userID + ", ");
+			System.out.println("Please choose from the options below:");
+			System.out.println("1. Search by category");
+			System.out.println("2. Display our inventory");
+			System.out.println("3. Return");
+			System.out.println("4. Donate book");
+			System.out.println("5. Quit");
 
-		userChoice = scan.nextInt();
+			userChoice = scan.nextInt();
 			// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 			if (userChoice == 1) {
 				int searchChoice = Validator.getInt(scan, "1. Search by author\n2. Search by title keyword: ", 1, 2);
@@ -28,12 +29,17 @@ public class LibraryApp {
 				if (searchChoice == 1) {
 					String author = Validator.getString(scan, "Please enter author: ");
 					ActionMethod.researchAuthor(author, userID);
-
+					int bookIDNum = Validator.getInt(scan, "\nPlease enter the bookID number you want to borrow: ");
+					ActionMethod.searchCheckout(bookIDNum, userID);
+					System.out.println("The book has been checked out.");
 				}
 
 				else if (searchChoice == 2) {
 					String keyword = Validator.getString(scan, "Please enter title keyword: ");
 					ActionMethod.researchKeyword(keyword, userID);
+					int bookIDNum = Validator.getInt(scan, "\nPlease enter the bookID number you want to borrow: ");
+					ActionMethod.searchCheckout(bookIDNum, userID);
+					System.out.println("The book has been checked out.");
 
 				} else {
 					System.out.println("The choice you entered is not avaiable");
@@ -54,6 +60,7 @@ public class LibraryApp {
 					count = Validator.getString(scan, "Would you like to borrow another book? (y/n)");
 
 				}
+			
 			}
 
 			// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -62,12 +69,13 @@ public class LibraryApp {
 				int bookID = Validator.getInt(scan, "What book would you like to return? (enter BookID): ", 100, 111);
 
 				ActionMethod.returnMethod(userID, bookID);
-				System.out.println();
+				System.out.println("Please put the book on the shelf. Thank you!\n");
 			}
 			if (userChoice == 4) {
 				String userString = Validator.getString(scan, "Enter a book and author:(name,author)");
 				ActionMethod.addBook(userString);
-				
+				System.out.println("Thank you for donating books.");
+
 			}
 		}
 		System.out.println("Thank you for visiting our library~!");
